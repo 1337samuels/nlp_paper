dab8f4daba729fe63a6732872f4ea811474189d5
Uri Goodman <uri.goodman@gmail.com>
2022-07-19 21:48:08 +0300

    feat: added test data creator

---
+++ code/create_test_data.py
@@ -0,0 +1,241 @@
+import argparse
+from random import choices
+import re
+import string
+from unicodedata import category
+from torch import cartesian_prod
+import tqdm
+from sklearn.model_selection import train_test_split
+import os
+import json
+
+# https://arxiv.org/help/api/user-manual
+CATEGORY_MAP = {'astro-ph': 'Astrophysics',
+                'astro-ph.CO': 'Cosmology and Nongalactic Astrophysics',
+                'astro-ph.EP': 'Earth and Planetary Astrophysics',
+                'astro-ph.GA': 'Astrophysics of Galaxies',
+                'astro-ph.HE': 'High Energy Astrophysical Phenomena',
+                'astro-ph.IM': 'Instrumentation and Methods for Astrophysics',
+                'astro-ph.SR': 'Solar and Stellar Astrophysics',
+                'cond-mat.dis-nn': 'Disordered Systems and Neural Networks',
+                'cond-mat.mes-hall': 'Mesoscale and Nanoscale Physics',
+                'cond-mat.mtrl-sci': 'Materials Science',
+                'cond-mat.other': 'Other Condensed Matter',
+                'cond-mat.quant-gas': 'Quantum Gases',
+                'cond-mat.soft': 'Soft Condensed Matter',
+                'cond-mat.stat-mech': 'Statistical Mechanics',
+                'cond-mat.str-el': 'Strongly Correlated Electrons',
+                'cond-mat.supr-con': 'Superconductivity',
+                'cs.AI': 'Artificial Intelligence',
+                'cs.AR': 'Hardware Architecture',
+                'cs.CC': 'Computational Complexity',
+                'cs.CE': 'Computational Engineering, Finance, and Science',
+                'cs.CG': 'Computational Geometry',
+                'cs.CL': 'Computation and Language',
+                'cs.CR': 'Cryptography and Security',
+                'cs.CV': 'Computer Vision and Pattern Recognition',
+                'cs.CY': 'Computers and Society',
+                'cs.DB': 'Databases',
+                'cs.DC': 'Distributed, Parallel, and Cluster Computing',
+                'cs.DL': 'Digital Libraries',
+                'cs.DM': 'Discrete Mathematics',
+                'cs.DS': 'Data Structures and Algorithms',
+                'cs.ET': 'Emerging Technologies',
+                'cs.FL': 'Formal Languages and Automata Theory',
+                'cs.GL': 'General Literature',
+                'cs.GR': 'Graphics',
+                'cs.GT': 'Computer Science and Game Theory',
+                'cs.HC': 'Human-Computer Interaction',
+                'cs.IR': 'Information Retrieval',
+                'cs.IT': 'Information Theory',
+                'cs.LG': 'Machine Learning',
+                'cs.LO': 'Logic in Computer Science',
+                'cs.MA': 'Multiagent Systems',
+                'cs.MM': 'Multimedia',
+                'cs.MS': 'Mathematical Software',
+                'cs.NA': 'Numerical Analysis',
+                'cs.NE': 'Neural and Evolutionary Computing',
+                'cs.NI': 'Networking and Internet Architecture',
+                'cs.OH': 'Other Computer Science',
+                'cs.OS': 'Operating Systems',
+                'cs.PF': 'Performance',
+                'cs.PL': 'Programming Languages',
+                'cs.RO': 'Robotics',
+                'cs.SC': 'Symbolic Computation',
+                'cs.SD': 'Sound',
+                'cs.SE': 'Software Engineering',
+                'cs.SI': 'Social and Information Networks',
+                'cs.SY': 'Systems and Control',
+                'econ.EM': 'Econometrics',
+                'eess.AS': 'Audio and Speech Processing',
+                'eess.IV': 'Image and Video Processing',
+                'eess.SP': 'Signal Processing',
+                'gr-qc': 'General Relativity and Quantum Cosmology',
+                'hep-ex': 'High Energy Physics - Experiment',
+                'hep-lat': 'High Energy Physics - Lattice',
+                'hep-ph': 'High Energy Physics - Phenomenology',
+                'hep-th': 'High Energy Physics - Theory',
+                'math.AC': 'Commutative Algebra',
+                'math.AG': 'Algebraic Geometry',
+                'math.AP': 'Analysis of PDEs',
+                'math.AT': 'Algebraic Topology',
+                'math.CA': 'Classical Analysis and ODEs',
+                'math.CO': 'Combinatorics',
+                'math.CT': 'Category Theory',
+                'math.CV': 'Complex Variables',
+                'math.DG': 'Differential Geometry',
+                'math.DS': 'Dynamical Systems',
+                'math.FA': 'Functional Analysis',
+                'math.GM': 'General Mathematics',
+                'math.GN': 'General Topology',
+                'math.GR': 'Group Theory',
+                'math.GT': 'Geometric Topology',
+                'math.HO': 'History and Overview',
+                'math.IT': 'Information Theory',
+                'math.KT': 'K-Theory and Homology',
+                'math.LO': 'Logic',
+                'math.MG': 'Metric Geometry',
+                'math.MP': 'Mathematical Physics',
+                'math.NA': 'Numerical Analysis',
+                'math.NT': 'Number Theory',
+                'math.OA': 'Operator Algebras',
+                'math.OC': 'Optimization and Control',
+                'math.PR': 'Probability',
+                'math.QA': 'Quantum Algebra',
+                'math.RA': 'Rings and Algebras',
+                'math.RT': 'Representation Theory',
+                'math.SG': 'Symplectic Geometry',
+                'math.SP': 'Spectral Theory',
+                'math.ST': 'Statistics Theory',
+                'math-ph': 'Mathematical Physics',
+                'nlin.AO': 'Adaptation and Self-Organizing Systems',
+                'nlin.CD': 'Chaotic Dynamics',
+                'nlin.CG': 'Cellular Automata and Lattice Gases',
+                'nlin.PS': 'Pattern Formation and Solitons',
+                'nlin.SI': 'Exactly Solvable and Integrable Systems',
+                'nucl-ex': 'Nuclear Experiment',
+                'nucl-th': 'Nuclear Theory',
+                'physics.acc-ph': 'Accelerator Physics',
+                'physics.ao-ph': 'Atmospheric and Oceanic Physics',
+                'physics.app-ph': 'Applied Physics',
+                'physics.atm-clus': 'Atomic and Molecular Clusters',
+                'physics.atom-ph': 'Atomic Physics',
+                'physics.bio-ph': 'Biological Physics',
+                'physics.chem-ph': 'Chemical Physics',
+                'physics.class-ph': 'Classical Physics',
+                'physics.comp-ph': 'Computational Physics',
+                'physics.data-an': 'Data Analysis, Statistics and Probability',
+                'physics.ed-ph': 'Physics Education',
+                'physics.flu-dyn': 'Fluid Dynamics',
+                'physics.gen-ph': 'General Physics',
+                'physics.geo-ph': 'Geophysics',
+                'physics.hist-ph': 'History and Philosophy of Physics',
+                'physics.ins-det': 'Instrumentation and Detectors',
+                'physics.med-ph': 'Medical Physics',
+                'physics.optics': 'Optics',
+                'physics.plasm-ph': 'Plasma Physics',
+                'physics.pop-ph': 'Popular Physics',
+                'physics.soc-ph': 'Physics and Society',
+                'physics.space-ph': 'Space Physics',
+                'q-bio.BM': 'Biomolecules',
+                'q-bio.CB': 'Cell Behavior',
+                'q-bio.GN': 'Genomics',
+                'q-bio.MN': 'Molecular Networks',
+                'q-bio.NC': 'Neurons and Cognition',
+                'q-bio.OT': 'Other Quantitative Biology',
+                'q-bio.PE': 'Populations and Evolution',
+                'q-bio.QM': 'Quantitative Methods',
+                'q-bio.SC': 'Subcellular Processes',
+                'q-bio.TO': 'Tissues and Organs',
+                'q-fin.CP': 'Computational Finance',
+                'q-fin.EC': 'Economics',
+                'q-fin.GN': 'General Finance',
+                'q-fin.MF': 'Mathematical Finance',
+                'q-fin.PM': 'Portfolio Management',
+                'q-fin.PR': 'Pricing of Securities',
+                'q-fin.RM': 'Risk Management',
+                'q-fin.ST': 'Statistical Finance',
+                'q-fin.TR': 'Trading and Market Microstructure',
+                'quant-ph': 'Quantum Physics',
+                'stat.AP': 'Applications',
+                'stat.CO': 'Computation',
+                'stat.ME': 'Methodology',
+                'stat.ML': 'Machine Learning',
+                'stat.OT': 'Other Statistics',
+                'stat.TH': 'Statistics Theory'}
+
+
+def get_metadata(data_file):
+    with open(data_file, 'r') as f:
+        for line in f:
+            yield line
+
+
+def spacing_punctuation(text):
+    regular_punct = list(string.punctuation)
+    for punc in regular_punct:
+        if punc in text:
+            text = text.replace(punc, f' {punc} ')
+    return text.strip()
+
+
+def clean_abstracts(abstract_texts):
+    abstracts = list(abstract_texts)
+    abstracts = map(str.lower, abstracts)
+    abstracts = map(lambda x: re.sub(
+        r'\$[^\$]\$', '', x), abstracts)  # Remove math
+    abstracts = map(spacing_punctuation, abstracts)
+    abstracts = map(lambda x: re.sub(
+        r'\\\s+[a-z]+\s+\{([^}]+)\}', '\\1', x), abstracts)  # consolidate whitespace
+    abstracts = map(lambda x: re.sub(r'\s+', ' ', x),
+                    abstracts)  # consolidate whitespace
+    abstracts = filter(lambda x: x.count(' ') < 150, abstracts)
+    return abstracts
+
+
+def get_abstracts(data_file, category):
+    abstract_texts = set()
+    for paper in tqdm.tqdm(get_metadata(data_file)):
+        paper_dict = json.loads(paper)
+        cats = paper_dict.get('categories').split(" ")
+        abstract = paper_dict.get('abstract')
+        if abstract not in abstract_texts and category in cats:
+            abstract_texts.add(abstract)
+            yield paper_dict.get('abstract')
+
+
+def write_abstracts(cleaned_abstracts, output_dir):
+    train_abstracts, test_abstracts = train_test_split(
+        cleaned_abstracts, test_size=0.1, random_state=42)
+    with open(os.path.join(output_dir, "test.txt"), 'w') as f:
+        f.writelines(map(lambda x: x + '\n', test_abstracts))
+    with open(os.path.join(output_dir, "train.txt"), 'w') as f:
+        f.writelines(map(lambda x: x + '\n', train_abstracts))
+
+
+def create_dataset(data_file: str, target_category: str, output_dir: str):
+    print('Reading from:', data_file)
+    print('Using category:', CATEGORY_MAP[target_category])
+    print('Output dir:', output_dir)
+    abstract_texts = get_abstracts(data_file, target_category)
+    cleaned_abstracts = clean_abstracts(abstract_texts)
+    write_abstracts(cleaned_abstracts, output_dir)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    # Required parameters
+    parser.add_argument("--category", default=None, type=str, required=True,
+                        choices=CATEGORY_MAP.keys(),
+                        help="The category to fine tune the model.")
+    parser.add_argument("--arxiv-data-file", default=None, type=str,
+                        help="The file in which the metadata is stored")
+    parser.add_argument("--output-dir", default=None, type=str,
+                        help="The directory in which to store the output")
+    args = parser.parse_args()
+    create_dataset(args.data_file, args.category, args.output_dir)
+
+
+if __name__ == "__main__":
+    main()
